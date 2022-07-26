c148e71e7dcc8ab6f0603b17fa46214c09049b08
Captain ALM <captainalm@captainalm.com>
2022-07-26 19:49:32 +0100

    .

--- pageHandler/pages/index/entry.go
+++ pageHandler/pages/index/entry.go
@@ -3,6 +3,7 @@ package index
 import (
 	"golang.captainalm.com/cityuni-webserver/utils/yaml"
 	"html/template"
+	"math"
 	"time"
 )
 
@@ -17,6 +18,11 @@ type EntryYaml struct {
 	VideoContentType   string        `yaml:"videoContentType"`
 	ThumbnailLocations []string      `yaml:"thumbnailLocations"`
 	ImageLocations     []string      `yaml:"imageLocations"`
+}
+
+type ImageReference struct {
+	ThumbnailLocation string
+	ImageLocation     string
 }
 
 func (ey EntryYaml) GetStartDate() string {
@@ -45,4 +51,19 @@ func (ey EntryYaml) GetContent() template.HTML {
 
 func (ey EntryYaml) GetDuration() time.Duration {
 	return ey.GetEndTime().Sub(ey.StartDate.Time).Truncate(time.Second)
+}
+
+func (ey EntryYaml) GetImageCount() int {
+	return int(math.Min(float64(len(ey.ThumbnailLocations)), float64(len(ey.ImageLocations))))
+}
+
+func (ey EntryYaml) GetImages() []ImageReference {
+	toReturn := make([]ImageReference, ey.GetImageCount())
+	for i := 0; i < len(ey.ThumbnailLocations) && i < len(ey.ImageLocations); i++ {
+		toReturn[i] = ImageReference{
+			ThumbnailLocation: ey.ThumbnailLocations[i],
+			ImageLocation:     ey.ImageLocations[i],
+		}
+	}
+	return toReturn
 }
